ca3f654d34daa0361c3375b861bf05fd819a7854
Yin <64zhangyin@163.com>
2018-03-25 11:49:15 -0500

    using index.php instead of html.php

---
+++ header.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+ ?>
+<!ODCTYPE html>
+<html>
+    <head>
+        <title></title>
+
+        <link  rel="stylesheet" type="text/css" href="style.css">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    </head>
+
+
+        <body>
+            <header>
+                <nav>
+                <div class="main-warpper">
+                    <ul>
+                        <li><a href="index.php" > TripKnitter</a></li>
+                    </ul>
+                    <div class="nav-login">
+
+                        <?php
+                            if (isset($_SESSION['u_id'])){
+                                echo '<form action="../homepage/includes/logout.inc.php" method="POST">
+                            <button type="submit" name="submit">Logout</button>
+                        </form>';
+                            } else{
+                                echo '<form action="../homepage/includes/login.inc.php" method="POST">
+                                    <input type="text" name="uid" placeholder="Email">
+                                     <input type="password" name="pwd" placeholder="Password">
+                                    <button type="submit" name="submit">Login </button>
+                                    <button type="button" name="submit" onclick="javascript:window.location.href=\'/homepage/signup.php\'">Sign up</button>
+                                </form>';
+                            }
+                        ?>
+
+
+                    </div>
+                </div>
+            </nav>
+            </header>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+	include "header.php";
+ ?>
+
+        <section class="main-container">
+        	<div class="main-wrapper">
+
+        	</div>
+        </section>
+            <div id="background" style="position:absolute;z-index:-1;width:100%;height:100%;left:0px;">
+                <img src="figures/1.1.png" width="100%" height="100%">
+            </div>
+
+
+            <div class="container">
+                <form id="search-homepage" action="">
+                    <div class="input-group input-group-lg" >
+                        <input type="text" class="form-control" placeholder="Search for Attractions" name="search-homepage">
+                        <span class="input-group-btn">
+                        <button type="submit" class="btn btn-lg" value="submit">
+                        Search</button>
+                        </span>
+
+                    </div>
+                </form>
+            </div>
+
+						<div class="section" style="margin-top:20%;padding:80px;">
+							<a href="index.php" style="padding-left:23%;">
+							<img src="figures/spots.png" alt="List of Spots" width="150px" height="150px" style="opacity:0.8"/>
+							</a>
+
+							<a href="index.php" style="padding-left:200px;">
+							<img src="figures/plan.png"  alt="Desgin Your Trip" width="150px" height="150px" style="opacity:0.8"/>
+							</a>
+
+							<br><br>
+							<span class="h3" style="padding-left:23%;">List of Spots</span>
+							<span class="h3" style="padding-left:180px;">Desgin Your Trip</span>
+						</div>
+
+<?php include "footer.php";
+ ?>

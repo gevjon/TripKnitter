b85aba03934efa4fd6bd941de1d374d8a1acf1d8
Yin <64zhangyin@163.com>
2018-03-27 00:22:40 -0500

    03/27

--- search_result.php
+++
@@ -1,7 +0,0 @@
-<div class="spot-box">
-  <img src="figures/spot_default.jpg" alt="spots image" width="200px" height="150px" style="padding:50px;float:left;">
-  <div style="float:left;">
-    <h2 style="padding-top:50px;"> This is a title </h2>
-    <p style=""> review_essential ...</p>
-  </div>
-</div>
